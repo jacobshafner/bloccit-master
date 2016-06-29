b3da8a52b84a65d561893445d2c67e3c1210a906
Jacob Hafner <jhafner1990@gmail.com>
2016-06-29 13:29:41 -0700

    completed checkpoint 43 assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,8 @@ class Post < ActiveRecord::Base
 
   default_scope { order('rank DESC')}
 
+	after_create :create_vote
+
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
@@ -30,5 +32,9 @@ class Post < ActiveRecord::Base
 		age_in_days = (created_at - Time.new(1970,1,1))/1.day.seconds
 		new_rank = points + age_in_days
 		update_attribute(:rank, new_rank)
+	end
+
+	def create_vote
+		user.votes.create(value: 1, post: self)
 	end
 end
